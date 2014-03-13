af9332bbf2220fd9d72137722394def96f38a2f4
bloomingbridges <brueckner.florian@googlemail.com>
2014-03-13 17:00:23 +0100

    Parsing Document line by line.

--- parser.js
+++ parser.js
@@ -1,4 +1,6 @@
 
+var DEBUG = true;
+
 // Dependencies ///////////////////////////////////////////////////////////////
 
 var fm = require("front-matter")
@@ -15,6 +17,19 @@ var CVMLParser = function() {
   };
 };
 
+CVMLParser.prototype.modifiers = {
+     PLAIN: "   ",
+     LABEL: "  -",
+  EMPHASIS: " --",
+   SECTION: "===",
+      RULE: "  =",
+     TITLE: "  /"
+};
+
+CVMLParser.prototype.identifyModifier = function(stub) {
+
+}
+
 CVMLParser.prototype.parseMetaData = function(document) {
   var content = fm(document);
   for (a in content.attributes) {
@@ -24,9 +39,40 @@ CVMLParser.prototype.parseMetaData = function(document) {
 };
 
 CVMLParser.prototype.parseContents = function(contents) {
-  var lines = contents.split("\n");
+  var lines = contents.split("\n")
+    , sections = []
+    , l = 0;
+
   console.log("Lines of CVML to parse:", lines.length);
+
+  while (l < lines.length) {
+    var line = this.parseLine(lines[l]);
+    l++;
+  }
   return true;
+};
+
+CVMLParser.prototype.parseLine = function(line) {
+
+  var type = "UNRECOGNISED"
+    , content = "";
+
+  if (line.length > 3) {
+    type = this.identifyModifier( line.substring(0,3) );
+    content = line.substr(4);
+
+    if (DEBUG) {
+      console.log("");
+      console.log("##############################################################");
+      console.log(content);
+      console.log("--------------------------------------------------------------");
+      console.log("TYPE: " + type + ", Length: " + content.length + " characters.")
+      console.log("==============================================================");
+      console.log("");
+    }
+
+    return { type: type, content: content };
+  }
 };
 
 
